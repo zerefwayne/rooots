2cb1cbcf7b0be19d555c711f2936ebe1b19423da
Aayush Joglekar <aayushjog@gmail.com>
2023-08-02 22:55:57 +0200

    Connect server to database

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/zerefwayne/rooots
 
 go 1.20
+
+require github.com/lib/pq v1.10.9
--- server/main.go
+++ server/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "roootsadmin"
+	password = "roootspw"
+	dbname   = "roootsdb"
 )
 
 func main() {
-	log.Println("Hello world!")
+
+	pqConnectionString := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	log.Println(pqConnectionString)
+
+	db, err := sql.Open("postgres", pqConnectionString)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 }
